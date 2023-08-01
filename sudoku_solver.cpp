d1cfdeaedf52b5ea24a97caa4cf1ac62850a55aa
tali <apalchik@uwaterloo.ca>
2023-08-01 12:16:33 -0400

    clarified display messaging

--- sudoku_solver.cpp
+++ sudoku_solver.cpp
@@ -57,6 +57,7 @@ SudokuGrid::SudokuGrid()
     inputFile.close();
 
     // Outputting the grid for user display
+    cout << "Your entered grid:" << endl;
     for (int i = 0; i < 9; i++)
     {
         for (int j = 0; j < 9; j++)
